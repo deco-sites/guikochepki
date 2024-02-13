55198ecc59571abc330baf9d1eb79729440eb6b5
GuilhermeKochepkiLiveSEO <guilherme.kochepki@liveseo.com.br>
2024-02-13 07:46:46 -0300

    fix: eslint

--- sections/Content/HeroSEO.tsx
+++ sections/Content/HeroSEO.tsx
@@ -14,7 +14,6 @@ export default function HeroFlats({
   activeReadMore,
   description,
 }: Props) {
-
   return (
     <div>
       <div className="mx-auto flex flex-col items-center gap-8 container sm:py-10">
@@ -25,7 +24,8 @@ export default function HeroFlats({
         )}
         {description && (
           <div id="hero-text">
-            {activeReadMore ? (
+            {activeReadMore
+              ? (
                 <div class="max-w-[1220px] flex gap-[30px] relative mx-8 md:mx-auto">
                   <details className="peer absolute bottom-0 translate-y-full group ">
                     <summary className="text-sm text-neutral-1 font-lato py-2 px-6 border border-neutral-1 cursor-pointer list-none">
@@ -35,17 +35,24 @@ export default function HeroFlats({
                   </details>
                   <div className="text-grey-500 h-[128px] peer-open:h-auto overflow-hidden pb-5 description">
                     <div>
-                      <div className="ml-2 mt-2 flex flex-col gap-2" dangerouslySetInnerHTML={{ __html: description }} />
+                      <div
+                        className="ml-2 mt-2 flex flex-col gap-2"
+                        dangerouslySetInnerHTML={{ __html: description }}
+                      />
                     </div>
                   </div>
                   <div class="h-10 w-[calc(100%-40px)] bg-gradient-to-b from-transparent to-white absolute bottom-0">
                   </div>
                 </div>
-            ) : (
-              <div className="max-w-[1220px] flex gap-[30px] relative mx-8 md:mx-auto">
-                <div className="text-16px md:text-18px leading-150%" dangerouslySetInnerHTML={{ __html: description }} />
-              </div>
-            )}
+              )
+              : (
+                <div className="max-w-[1220px] flex gap-[30px] relative mx-8 md:mx-auto">
+                  <div
+                    className="text-16px md:text-18px leading-150%"
+                    dangerouslySetInnerHTML={{ __html: description }}
+                  />
+                </div>
+              )}
           </div>
         )}
       </div>
